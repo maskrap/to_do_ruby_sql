43f4cae16b31b521cfc3247fa6acec5ea3e71b9a
Sam Park <sampark18@gmail.com>
2016-07-18 06:23:10 -0700

    Following along homework, up to queries

--- app.rb
+++ app.rb
@@ -1,6 +1,31 @@
+require("sinatra/activerecord")
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
-require("sinatra/activerecord")
 require("./lib/task")
 require("pg")
+
+get('/') do
+  @tasks = Task.all()
+  erb(:index)
+end
+
+post('tasks') do
+  description = params.fetch('description')
+  task = Task.new({:description => description, :done => false})
+  task.save()
+  erb(:success)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
+  erb(:index)
+end
